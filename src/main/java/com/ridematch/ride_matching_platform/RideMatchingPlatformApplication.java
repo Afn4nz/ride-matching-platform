e9123af49c3c5e56e8f627addc8608788c74cc98
aalzuayr <aalzuayr@tahakom.com>
2025-09-18 21:39:10 +0300

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/ridematch/ride_matching_platform/RideMatchingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ridematch.ride_matching_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideMatchingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideMatchingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ridematch/ride_matching_platform/RideMatchingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridematch.ride_matching_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideMatchingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
